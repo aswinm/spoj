8cc1bac09a234cac6257608b6daf74bc2518e0e8
aswin.m <aswinmurugesh94@gmail.com>
2013-09-15 11:00:39 +0530

    initial commit

---
+++ EDIT.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+
+int main(){
+    char s[1001];
+    int u=0,l=0,t;
+    while(scanf("%s",s)!=EOF){
+        //printf("%s",s);
+        int z=strlen(s);
+        for(int i=0;i<z;i++){
+            t=i%2;
+            if(s[i]>='a') 
+            	if(t==0) 
+            		u++; 
+            	else 
+            		l++;
+            if(s[i]<'a') 
+            	if(t==0) 
+            		l++; 
+            	else 
+            		u++;
+            
+        }
+        if(u<l)
+            printf("%d\n",u);
+        else
+            printf("%d\n",l);
+        u=0;
+        l=0;
+    }
+    return 0;
+}
+
