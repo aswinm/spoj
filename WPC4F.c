e3086ef9e5b8b63c575e15a07d803880a6415700
Aswin Murugesh <asksm46@gmail.com>
2014-02-02 11:29:40 +0000

    Update WPC4F.c

--- WPC4F.c
+++ WPC4F.c
@@ -16,20 +16,16 @@ main()
 				scanf("%d",&s[i][j]);
 		for(i=1;i<n;i++)
 		{
-			for(j=0;j<3;j++)
-			{
-				if(!j)
-				s[i][j] += min(s[i-1][1],s[i-1][2]);	
-				else if(j==1)
-				s[i][j] += min(s[i-1][0],s[i-1][2]);
-				else
-				s[i][j] += min(s[i-1][0],s[i-1][1]);
-			}
+			
+				s[i][0] += min(s[i-1][1],s[i-1][2]);	
+				
+				s[i][1] += min(s[i-1][0],s[i-1][2]);
+				
+				s[i][2] += min(s[i-1][0],s[i-1][1]);
+			
 		}
-		mi = 30000;
-		for(i=0;i<3;i++)
-			if(s[n-1][i]<mi)
-				mi = s[n-1][i];
+		mi = min(s[n-1][0],min(s[n-1][1],s[n-1][2]));
+		
 		printf("%d\n",mi);
 	}
 	return 0;
